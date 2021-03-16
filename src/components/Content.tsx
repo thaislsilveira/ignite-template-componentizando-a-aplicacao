f23588d3930b7f37a942fe9b0c5950107d4755b1
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-16 08:55:11 -0300

    ending challenge

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,41 @@
-export function Content() {
-  // Complete aqui
+import '../styles/content.scss';
+import { MovieCard } from './MovieCard';
+
+interface ContentProps {
+  movies: Array<MovieProps>;
+  selectedGenre: {
+    title: string;
+  }
+}
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content(props: ContentProps) {
+
+  const { movies, selectedGenre } = props;
+
+
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: Array<GenreProps>;
+  handleClickButton: Function;
+  selectedGenreId: number;
+
+}
+
+interface GenreProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar(props: SideBarProps) {
+
+  const { genres, handleClickButton, selectedGenreId } = props;
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          id={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>
+  )
 }
